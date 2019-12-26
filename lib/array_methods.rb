21133345796fbc38f62dd253188bb05fc0204d5a
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 02:24:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,21 +3,21 @@ array.include?(element)
 end
 
 def using_sort(array)
-array.sort(element)
+array.sort
 end
 
 def using_reverse(array)
-array.reverse(element)
+array.reverse
 end
 
 def using_first(array)
-array.first(element)
+array.first
 end
 
 def using_last(array)
-array.last(element)
+array.last
 end
 
 def using_size(array)
-array.size(element)
+array.size
 end
