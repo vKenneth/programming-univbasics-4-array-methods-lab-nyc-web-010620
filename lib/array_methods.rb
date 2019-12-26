18c5b6d8da3edd23da59eff6e64f9ee73b58bec6
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 02:24:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,23 @@
 def using_include(array, element)
-include?
+array.include?(element)
 end
 
 def using_sort(array)
-
+array.sort(element)
 end
 
 def using_reverse(array)
-
+array.reverse(element)
 end
 
 def using_first(array)
-
+array.first(element)
 end
 
 def using_last(array)
-
+array.last(element)
 end
 
 def using_size(array)
-
+array.size(element)
 end
